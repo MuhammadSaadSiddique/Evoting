b0894457d8c3b984faf56cc3c9eeae0b8b13904d
Muhammad Saad Siddique <26270646+MuhammaadSaad@users.noreply.github.com>
2022-06-25 17:40:42 +0500

    deploy contract using hard hat

--- smart-contract/scripts/sample-script.js
+++ smart-contract/scripts/sample-script.js
@@ -14,12 +14,12 @@ async function main() {
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const Evote = await hre.ethers.getContractFactory("Greeter");
+  const evote = await Evote.deploy();
 
-  await greeter.deployed();
+  await evote.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("voting deployed to:", evote.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
